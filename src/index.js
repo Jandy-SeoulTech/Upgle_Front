9117a24345111250aef1965bb67e53ea04cf9f9d
Inseo Hwang <sjsjsj1246@gmail.com>
2021-08-14 02:58:00 +0900

    feat: 폰트 적용, 영문 Roboto, 한글 Noto Sans KR

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { applyMiddleware, createStore } from "redux";
-import rootReducer, { rootSaga } from "./modules";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Provider } from "react-redux";
-import logger from "redux-logger";
-import createSagaMiddleware from "redux-saga";
-import { HelmetProvider } from "react-helmet-async";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer, { rootSaga } from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { HelmetProvider } from 'react-helmet-async';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import '@fontsource/noto-sans-kr';
+import '@fontsource/roboto';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
+  composeWithDevTools(applyMiddleware(sagaMiddleware, logger)),
 );
 
 sagaMiddleware.run(rootSaga);
@@ -21,8 +24,9 @@ sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <HelmetProvider>
+      <CssBaseline />
       <App />
     </HelmetProvider>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
