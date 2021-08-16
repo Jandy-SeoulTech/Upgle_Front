c81a3f7958378a6e2de428df066b596fb06c252a
Inseo Hwang <sjsjsj1246@gmail.com>
2021-08-16 21:10:47 +0900

    feat: main 페이지 더미 데이터 로딩 방식 수정

--- src/containers/common/MainContainer.jsx
+++ src/containers/common/MainContainer.jsx
@@ -1,17 +1,46 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Main from "../../components/common/Main";
-import { getPostList } from "../../modules/posts";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Main from '../../components/common/Main';
+import { getPostList } from '../../modules/posts';
 
 const MainContainer = (props) => {
-  const { postList } = useSelector((state) => state.posts);
+  const [postList] = useState([
+    {
+      id: 1,
+      title: '더미 데이터1',
+      body: '비상계엄하의 군사재판은 군인·군무원의 범죄나 군사에 관한 간첩죄의 경우와 초병·초소·유독음식물공급·포로에 관한 죄중 법률이 정한 경우에 한하여 단심으로 할 수 있다. 다만, 사형을 선고한 경우에는 그러하지 아니하다. 대통령으로 선거될 수 있는 자는 국회의원의 피선거권이 있고 선거일 현재 40세에 달하여야 한다.',
+    },
+    {
+      id: 2,
+      title: '더미 데이터2',
+      body: '법관은 헌법과 법률에 의하여 그 양심에 따라 독립하여 심판한다. 감사원의 조직·직무범위·감사위원의 자격·감사대상공무원의 범위 기타 필요한 사항은 법률로 정한다. 사회적 특수계급의 제도는 인정되지 아니하며, 어떠한 형태로도 이를 창설할 수 없다. 근로조건의 기준은 인간의 존엄성을 보장하도록 법률로 정한다.',
+    },
+    {
+      id: 3,
+      title: '더미 데이터3',
+      body: 'on eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret. nam quibusdam, et iis quidem non admodum indoctis, totum hoc displicet philosophari. quidam autem non tam id reprehendunt, si remissius agatur, sed tantum studium tamque multam operam ponendam in eo non arbitrantur. erunt etiam, et ii quidem eruditi Graecis litteris, contemnentes Latinas, qui se dicant in Graecis legendis operam malle consumere. postremo aliquos futuros suspicor, qui me ad alias litteras vocent, genus hoc scribendi, etsi sit elegans, personae tamen et dignitatis esse negent.',
+    },
+    {
+      id: 4,
+      title: '더미 데이터4',
+      body: '비상계엄하의 군사재판은 군인·군무원의 범죄나 군사에 관한 간첩죄의 경우와 초병·초소·유독음식물공급·포로에 관한 죄중 법률이 정한 경우에 한하여 단심으로 할 수 있다. 다만, 사형을 선고한 경우에는 그러하지 아니하다. 대통령으로 선거될 수 있는 자는 국회의원의 피선거권이 있고 선거일 현재 40세에 달하여야 한다.',
+    },
+    {
+      id: 5,
+      title: '더미 데이터5',
+      body: '법관은 헌법과 법률에 의하여 그 양심에 따라 독립하여 심판한다. 감사원의 조직·직무범위·감사위원의 자격·감사대상공무원의 범위 기타 필요한 사항은 법률로 정한다. 사회적 특수계급의 제도는 인정되지 아니하며, 어떠한 형태로도 이를 창설할 수 없다. 근로조건의 기준은 인간의 존엄성을 보장하도록 법률로 정한다.',
+    },
+    {
+      id: 6,
+      title: '더미 데이터6',
+      body: 'on eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret. nam quibusdam, et iis quidem non admodum indoctis, totum hoc displicet philosophari. quidam autem non tam id reprehendunt, si remissius agatur, sed tantum studium tamque multam operam ponendam in eo non arbitrantur. erunt etiam, et ii quidem eruditi Graecis litteris, contemnentes Latinas, qui se dicant in Graecis legendis operam malle consumere. postremo aliquos futuros suspicor, qui me ad alias litteras vocent, genus hoc scribendi, etsi sit elegans, personae tamen et dignitatis esse negent.',
+    },
+  ]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPostList());
   }, [dispatch]);
-
-  if (!postList) return <div>로딩중...</div>;
 
   return <Main postList={postList} />;
 };
