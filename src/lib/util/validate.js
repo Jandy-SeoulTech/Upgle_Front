fba79a799f1ed64ddfb7f8f85d02d27a8192b632
Inseo Hwang <sjsjsj1246@gmail.com>
2021-08-17 18:22:47 +0900

    fix: code

--- src/lib/util/validate.js
+++ src/lib/util/validate.js
@@ -9,19 +9,9 @@ export function isCode(asValue) {
   return regExp.test(asValue);
 }
 
-export function isCode(asValue) {
-  var regExp = /^[a-zA-Z0-9]*$/gm;
-  return regExp.test(asValue);
-}
-
 // 8 ~ 10자 영문, 숫자 조합 특수문자 입력가능
 export function isPassword(asValue) {
   var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[A-Za-z\d$@$!%*#?&]{8,16}$/;
-  return regExp.test(asValue);
-}
-
-export function isNickname(asValue) {
-  var regExp = /^[a-zA-Z0-9가-힣]{4,8}$/;
   return regExp.test(asValue);
 }
 
