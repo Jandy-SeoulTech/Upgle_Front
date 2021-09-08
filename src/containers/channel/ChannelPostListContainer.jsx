2a0de6837ef76eb51e37456e5bff2370f8824349
KeisLuv5991 <chmhj9997@gmail.com>
2021-09-08 20:18:40 +0900

    Feat: 채널 게시판 디자인 적용

--- src/containers/channel/ChannelPostListContainer.jsx
+++ src/containers/channel/ChannelPostListContainer.jsx
@@ -21,6 +21,53 @@ const ChannelPostListContainer = (props) => {
     {
       title: '가나다',
       content: '가나다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. 다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. 다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. 다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다.  첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. 가나다는 한국어 자음의 첫 세글자다. ',
+      status: 'Notice',
+      authorId: 1,
+      author: {
+        nickname: "제빵왕",
+        profile: {
+          profileImage: {
+            src: "admin.jpg"
+          }
+        }
+      },
+      updatedAt: "2021-09-08T11:48:50.171Z",
+    },
+    {
+      title: '가나다',
+      content: '가나다는 한국어 자음의 첫 세글자다. ',
+      status: 'Open',
+      authorId: 1,
+      author: {
+        nickname: "제빵왕",
+        profile: {
+          profileImage: {
+            src: "admin.jpg"
+          }
+        }
+      },
+      updatedAt: "2021-08-28T11:48:50.171Z",
+    },
+    {
+      title: '가나다',
+      content: '가나다는 한국어 자음의 첫 세글자다. ',
+      status: 'Reservation',
+      authorId: 2,
+      author: {
+        nickname: "제빵왕",
+        profile: {
+          profileImage: {
+            src: "admin.jpg"
+          }
+        }
+      },
+      updatedAt: "2021-08-28T11:48:50.171Z",
+    },
+    {
+      title: '가나다',
+      content: '가나다는 한국어 자음의 첫 세글자다. ',
+      status: 'Close',
+      authorId: 2,
       author: {
         nickname: "HSW",
         profile: {
@@ -34,45 +81,8 @@ const ChannelPostListContainer = (props) => {
     {
       title: '가나다',
       content: '가나다는 한국어 자음의 첫 세글자다. ',
-      author: {
-        nickname: "HSW",
-        profile: {
-          profileImage: {
-            src: "admin.jpg"
-          }
-        }
-      },
-      updatedAt: "2021-08-28T11:48:50.171Z",
-    },
-    {
-      title: '가나다',
-      content: '가나다는 한국어 자음의 첫 세글자다. ',
-      author: {
-        nickname: "HSW",
-        profile: {
-          profileImage: {
-            src: "admin.jpg"
-          }
-        }
-      },
-      updatedAt: "2021-08-28T11:48:50.171Z",
-    },
-    {
-      title: '가나다',
-      content: '가나다는 한국어 자음의 첫 세글자다. ',
-      author: {
-        nickname: "HSW",
-        profile: {
-          profileImage: {
-            src: "admin.jpg"
-          }
-        }
-      },
-      updatedAt: "2021-08-28T11:48:50.171Z",
-    },
-    {
-      title: '가나다',
-      content: '가나다는 한국어 자음의 첫 세글자다. ',
+      status: 'Close',
+      authorId: 2,
       author: {
         nickname: "HSW",
         profile: {
@@ -85,10 +95,29 @@ const ChannelPostListContainer = (props) => {
     },
   ];
 
+  const channel = {
+    "id": 1,
+    "name": "channel1",
+    "introduce": "test",
+    "adminId": 1,
+    "createdAt": "1970-01-01T00:00:00.000Z",
+    "updatedAt": null,
+    "admin": {
+      "id": 1,
+      "email": "iqeq1945@naver.com",
+      "nickname": "HSW",
+      "profile": {
+        "profileImage": {
+          "src": null
+        }
+      }
+    }
+  };
 
   return (
     <ChannelPostList
       postList={postList}
+      channel={channel}
     />
   );
 };
