3cde8113c4ba0f54464720ab6f2907f2fffae739
Inseo Hwang <sjsjsj1246@gmail.com>
2021-09-29 01:02:36 +0900

    Feat: 채팅방 생성, 예약 기능

--- src/components/channel/ChannelPost.jsx
+++ src/components/channel/ChannelPost.jsx
@@ -11,8 +11,9 @@ import {
   MenuItem,
   ListItemText,
   Divider,
+  Checkbox,
 } from '@material-ui/core';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Button from './../common/Button';
 import { ReactComponent as PostSetting } from '../../lib/assets/postSetting.svg';
 import { ReactComponent as LikeIcon } from '../../lib/assets/likeIcon.svg';
@@ -21,11 +22,26 @@ import { ReactComponent as ChatMake } from '../../lib/assets/chatMake.svg';
 import { ReactComponent as ChatGo } from '../../lib/assets/chatGo.svg';
 import { getDateString } from '../../lib/util/dateFormat';
 import palette from '../../lib/styles/palette';
-import TextField from './../common/TextField';
 import { Paper } from '@material-ui/core';
 import { useCallback } from 'react';
+import Modal from '../common/Modal';
+import { TextField as MuiTextField } from '@material-ui/core';
+import { TextField } from '../TextField';
+import Clear from '@material-ui/icons/Clear';
 
-const ChannelPost = ({ post, channel, user, isLiked, onLikeChannel, onUnLikeChannel }) => {
+const ChannelPost = ({
+  post,
+  channel,
+  user,
+  isLiked,
+  onLikeChannel,
+  onUnLikeChannel,
+  onCreateRoom,
+}) => {
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const [roomName, setRoomName] = useState('');
+  const [isReserve, setIsReserve] = useState(false);
+  const [reserveDate, setReserveDate] = useState();
   const [menuAnchor, setMenuAnchor] = useState();
   const handleMenu = (e) => {
     if (!e) return;
@@ -35,6 +51,14 @@ const ChannelPost = ({ post, channel, user, isLiked, onLikeChannel, onUnLikeChan
   const handleMoveChat = (roomId) => {
     window.open(`/chat/${roomId}`, '_blank', 'width=600, height=900, toolbars=no, scrollbars=yes');
     return false;
+  };
+
+  const handleCreateRoom = () => {
+    onCreateRoom({
+      status: isReserve ? 'Reservation' : 'Open',
+      name: roomName,
+      reservedTime: isReserve ? reserveDate : null,
+    });
   };
 
   const StatusIcon = ({ status }) => {
@@ -112,7 +136,12 @@ const ChannelPost = ({ post, channel, user, isLiked, onLikeChannel, onUnLikeChan
       case 'Close':
         ChatControllButton = ({}) => {
           return (
-            <Button className="makeChat">
+            <Button
+              className="makeChat"
+              onClick={() => {
+                setCreateModalOpen(true);
+              }}
+            >
               <ChatMake className="icon" />
               채팅방 만들기
             </Button>
@@ -202,6 +231,74 @@ const ChannelPost = ({ post, channel, user, isLiked, onLikeChannel, onUnLikeChan
           <Typography>{post.content}</Typography>
           <ControllButtonList status={post.status} />
         </Box>
+        <Modal open={createModalOpen} setOpen={setCreateModalOpen}>
+          <Paper css={createRoomModal}>
+            <Box css={modalTitle}>
+              <Typography>채팅방 만들기</Typography>
+              <Clear
+                onClick={() => {
+                  setCreateModalOpen(false);
+                }}
+              />
+            </Box>
+            <Box css={modalContent}>
+              <Box css={modalForm}>
+                <Typography>채팅방 이름</Typography>
+                <TextField
+                  size="small"
+                  variant="outlined"
+                  value={roomName}
+                  onChange={(e) => {
+                    setRoomName(e.target.value);
+                  }}
+                  maxLength={20}
+                  fullWidth
+                />
+              </Box>
+              <Box css={modalForm}>
+                <Typography>예약 하기</Typography>
+                <Checkbox
+                  checked={isReserve}
+                  onChange={(e) => {
+                    setIsReserve(e.target.checked);
+                  }}
+                />
+              </Box>
+              <Box css={modalForm}>
+                <Typography>예약 시간</Typography>
+                <MuiTextField
+                  type="datetime-local"
+                  sx={{ width: 300 }}
+                  value={reserveDate}
+                  disabled={!isReserve}
+                  onChange={(e) => {
+                    setReserveDate(e.target.value);
+                    console.log(reserveDate);
+                  }}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Box>
+              <Box css={modalButtonGroup}>
+                <Button
+                  css={modalButton}
+                  disabled={!roomName || isReserve}
+                  onClick={handleCreateRoom}
+                >
+                  바로 열기
+                </Button>
+                <Button
+                  css={modalButton}
+                  disabled={!roomName || !isReserve || !reserveDate}
+                  onClick={handleCreateRoom}
+                >
+                  예약 하기
+                </Button>
+              </Box>
+            </Box>
+          </Paper>
+        </Modal>
         <Grid container alignItems="center" css={postCommentWrapper}>
           <Typography className="postCommentHeader">댓글 {post.comment.length}</Typography>
           {post.comment.map((comment) => {
@@ -302,6 +399,89 @@ const postContentWrapper = css`
     letter-spacing: 0.2px;
     color: #000000;
   }
+`;
+
+const createRoomModal = css`
+  width: 50rem;
+  height: 28.125rem;
+  background-color: white;
+  border-radius: 10px;
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+`;
+
+const modalTitle = css`
+  height: 3.75rem;
+  padding: 0 1.34375rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+  .MuiTypography-root {
+    height: 100%;
+    line-height: 3.75rem;
+    font-family: 'Noto Sans KR';
+    font-weight: 700;
+    font-size: 1rem;
+  }
+`;
+
+const modalContent = css`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 7.5rem;
+`;
+
+const modalForm = css`
+  display: flex;
+  height: 2.25rem;
+  align-items: center;
+  .MuiTypography-root {
+    width: 6.875rem;
+    height: 100%;
+    flex-shrink: 0;
+    font-family: 'Noto Sans KR';
+    font-weight: 600;
+    font-size: 1rem;
+    display: flex;
+    align-items: center;
+  }
+  .MuiButtonBase-root {
+    margin: 0;
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+  .MuiFormControl-root {
+    height: 2.25rem;
+  }
+  & + & {
+    margin-top: 2.1875rem;
+  }
+`;
+
+const modalButtonGroup = css`
+  width: 100%;
+  margin-top: 4.375rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const modalButton = css`
+  width: 15.625rem;
+  height: 3.5625rem;
+  background: black;
+  color: white;
+  :disabled {
+    background: #e0e0e0;
+    color: #5f5f5f;
+  }
+  font-family: 'Noto Sans KR';
+  font-weight: 700;
+  font-size: 1.25rem;
+  border-radius: 10px;
 `;
 
 const controllButtonWrapper = css`
--- src/containers/channel/ChannelPostContainer.jsx
+++ src/containers/channel/ChannelPostContainer.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 import { getChannelData, getChannelPost } from '../../modules/channel';
+import { createRoom } from '../../modules/room';
 import ChannelPost from './../../components/channel/ChannelPost';
 
 const ChannelPostContainer = ({ channelId, postId }) => {
   const { channel, post } = useSelector((state) => state.channel);
   const { user } = useSelector((state) => state.user);
+  const history = useHistory();
+
+  const onCreateRoom = async ({ status, name, reservedTime }) => {
+    await dispatch(createRoom({ status, name, channelId, postId, reservedTime }));
+    history.push(`channel/${channelId}/chat`);
+  };
 
   const dispatch = useDispatch();
 
@@ -16,7 +24,7 @@ const ChannelPostContainer = ({ channelId, postId }) => {
 
   if (!channel || !post) return '로딩중';
 
-  return <ChannelPost post={post} channel={channel} user={user} />;
+  return <ChannelPost post={post} channel={channel} user={user} onCreateRoom={onCreateRoom} />;
 };
 
 export default ChannelPostContainer;
