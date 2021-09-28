b023a2ad5f2574a78f0c9a9a8241d3bf83500969
md2eoseo <md2eoseo@gmail.com>
2021-09-28 13:32:12 +0900

    style: 게시물 호버 시 배경색 스타일 추가

--- src/components/channel/PostCard.jsx
+++ src/components/channel/PostCard.jsx
@@ -30,6 +30,11 @@ const postCardWrapper = css`
   padding: 0.6875rem 1.5625rem;
   display: flex;
   justify-content: space-between;
+
+  :hover {
+    background-color: #f0f0f0;
+  }
+
   & + & {
     border-top: 1px solid #e0e0e0;
   }
