6a3c1b1d29407920b7f5a75e85531f9340b9a36f
md2eoseo <md2eoseo@gmail.com>
2021-09-28 13:13:46 +0900

    feat: 채널 홈 페이지 데이터 연동

--- src/containers/channel/ChannelHomeContainer.jsx
+++ src/containers/channel/ChannelHomeContainer.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ChannelHome from '../../components/channel/ChannelHome';
-import { getChannelData } from '../../modules/channel';
+import { getChannelData, getChannelPost, getRoomList } from '../../modules/channel';
 
 const ChannelHomeContainer = ({ channelId }) => {
-  const { channel } = useSelector((state) => state.channel);
+  const { channel, postList, roomList } = useSelector((state) => state.channel);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(getRoomList({ channelId }));
+    dispatch(getChannelPost(channelId));
     dispatch(getChannelData(channelId));
   }, [dispatch, channelId]);
 
   if (!channel) return '로딩중';
 
-  return <ChannelHome channel={channel} />;
+  return <ChannelHome channel={channel} postList={postList} roomList={roomList} />;
 };
 
 export default ChannelHomeContainer;
