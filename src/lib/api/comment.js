5c5b832b17355fcbe8fff9bf9d6e0222e61195e2
md2eoseo <md2eoseo@gmail.com>
2021-10-05 21:37:47 +0900

    fix: Comment API 명세 오류, 네이밍 수정

--- src/lib/api/comment.js
+++ src/lib/api/comment.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
 export const writeComment = async ({ channelId, postId, content }) => {
-    const response = await axios.post('/api/Comment', { channelId, postId, content });
-    return response.data.data;
+  const response = await axios.post('/api/Comment', { channelId, postId, content });
+  return response.data.data;
 };
 
 export const editComment = async ({ commentId, content }) => {
-    const response = await axios.patch(`/api/Comment/${commentId}`, { content });
-    return response.data.data;
+  const response = await axios.patch(`/api/Comment/${commentId}`, { content });
+  return response.data.data;
 };
 
-export const deleteComment = async ({ channelId, postId, content }) => {
-    const response = await axios.delete('/api/Comment', { channelId, postId, content });
-    return response.data.data;
+export const deleteComment = async ({ commentId }) => {
+  const response = await axios.delete(`/api/Comment/${commentId}`);
+  return response.data.data;
 };
-
--- src/modules/comment.js
+++ src/modules/comment.js
@@ -2,12 +2,12 @@ import { createAction, handleActions } from 'redux-actions';
 import { pender } from 'redux-pender/lib/utils';
 import * as commentAPI from '../lib/api/comment';
 
-const WRITE_COMMENT = 'comment/WRITE_COMMENT'
-const EDIT_COMMENT = 'comment/EDIT_COMMENT'
-const DELETE_COMMENT = 'comment/DELETE_COMMENT'
-const INIT_COMMENT = 'comment/INIT_COMMENT'
+const WRITE_COMMENT = 'comment/WRITE_COMMENT';
+const EDIT_COMMENT = 'comment/EDIT_COMMENT';
+const DELETE_COMMENT = 'comment/DELETE_COMMENT';
+const INIT_COMMENT = 'comment/INIT_COMMENT';
 
-export const weiteComment = createAction(WRITE_COMMENT, commentAPI.writeComment);
+export const writeComment = createAction(WRITE_COMMENT, commentAPI.writeComment);
 export const editComment = createAction(EDIT_COMMENT, commentAPI.editComment);
 export const deleteComment = createAction(DELETE_COMMENT, commentAPI.deleteComment);
 export const initialize = createAction(INIT_COMMENT);
@@ -24,7 +24,7 @@ export default handleActions(
       type: WRITE_COMMENT,
       onSuccess: (state, { payload: comment }) => ({
         ...state,
-        comment
+        comment,
       }),
     }),
   },
