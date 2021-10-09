9e937e8361a0052fc20c386549445ae3dc658399
md2eoseo <md2eoseo@gmail.com>
2021-10-09 18:57:01 +0900

    feat: 검색 페이지 뷰 작성

--- src/containers/common/HeaderContainer.jsx
+++ src/containers/common/HeaderContainer.jsx
@@ -4,7 +4,7 @@ import Header from '../../components/common/Header';
 import { logout } from '../../modules/user';
 import { useHistory } from 'react-router';
 
-const HeaderContainer = ({ initialKeyword = '' }) => {
+const HeaderContainer = () => {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const history = useHistory();
@@ -13,7 +13,7 @@ const HeaderContainer = ({ initialKeyword = '' }) => {
     dispatch(logout());
     history.push('/');
   };
-  return <Header user={user} initialKeyword={initialKeyword} onLogout={onLogout} />;
+  return <Header user={user} onLogout={onLogout} />;
 };
 
 export default HeaderContainer;
