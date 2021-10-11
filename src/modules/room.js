bb46f6c9175c7043f7d1c2dfcfed434dc248d2a8
Inseo Hwang <sjsjsj1246@gmail.com>
2021-10-11 20:07:27 +0900

    Refactor: room 모델 수정

--- src/modules/room.js
+++ src/modules/room.js
@@ -2,12 +2,12 @@ import { createAction, handleActions } from 'redux-actions';
 import { pender } from 'redux-pender/lib/utils';
 import * as roomAPI from '../lib/api/room';
 
-const CREATE_ROOM = 'chat/CREATE_ROOM';
-const GET_ROOM_DATA = 'chat/GET_ROOM_DATA';
-const GET_ROOM_LIST = 'chat/GET_ROOM_LIST';
-const EXIT_ROOM = 'chat/EXIT_ROOM';
-const CLOSE_ROOM = 'chat/CLOSE_ROOM';
-const REVIEW_ROOM = 'chat/REVIEW_ROOM';
+const CREATE_ROOM = 'room/CREATE_ROOM';
+const GET_ROOM_DATA = 'room/GET_ROOM_DATA';
+const GET_ROOM_LIST = 'room/GET_ROOM_LIST';
+const EXIT_ROOM = 'room/EXIT_ROOM';
+const CLOSE_ROOM = 'room/CLOSE_ROOM';
+const REVIEW_ROOM = 'room/REVIEW_ROOM';
 
 export const getRoomList = createAction(GET_ROOM_LIST, roomAPI.getRoomList);
 export const createRoom = createAction(CREATE_ROOM, roomAPI.createRoom);
@@ -24,12 +24,8 @@ const initialState = {
   userExit: null,
 };
 
-const chat = handleActions(
+export default handleActions(
   {
-    ...pender({
-      type: CREATE_ROOM,
-      onFailure: (state, { payload: error }) => ({ ...state, error }),
-    }),
     ...pender({
       type: GET_ROOM_DATA,
       onSuccess: (state, { payload }) => ({ ...state, room: payload }),
@@ -37,17 +33,6 @@ const chat = handleActions(
     ...pender({
       type: GET_ROOM_LIST,
       onSuccess: (state, { payload }) => ({ ...state, roomList: payload }),
-    }),
-    ...pender({
-      type: GET_ROOM_LIST,
-      onSuccess: (state, { payload }) => ({ ...state, roomList: payload }),
-      onFailure: (state, { payload: error }) => ({ ...state, error }),
-    }),
-    ...pender({
-      type: EXIT_ROOM,
-      onPending: (state) => ({ ...state, userExit: null }),
-      onSuccess: (state) => ({ ...state, userExit: true }),
-      onFailure: (state, { payload: error }) => ({ ...state, error }),
     }),
     ...pender({
       type: EXIT_ROOM,
@@ -70,5 +55,3 @@ const chat = handleActions(
   },
   initialState,
 );
-
-export default chat;
